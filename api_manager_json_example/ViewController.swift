838cf59dbfd99de2e02a462f65de83af92447a19
Ignacio Martínez Capella <nachetemc97@gmail.com>
2018-03-21 21:54:15 +0100

    Initial Commit

---
+++ api_manager_json_example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  api_manager_json_example
+//
+//  Created by Mariano Martinez on 21/3/18.
+//  Copyright © 2018 Nacho Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
